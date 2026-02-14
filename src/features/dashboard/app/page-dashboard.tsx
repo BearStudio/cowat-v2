2ac9e448dcd49b0a91020922e112b488a32d2d6a
Noe TATOUD <noe.tatoud@gmail.com>
2026-02-14 18:23:31 +0100

    feat: add app dashboard page and route with placeholder content

---
+++ src/features/dashboard/app/page-dashboard.tsx
@@ -0,0 +1,19 @@
+import {
+  PageLayout,
+  PageLayoutContent,
+  PageLayoutTopBar,
+  PageLayoutTopBarTitle,
+} from '@/layout/app/page-layout';
+
+export const PageDashboard = () => {
+  return (
+    <PageLayout>
+      <PageLayoutTopBar>
+        <PageLayoutTopBarTitle>Dashboard</PageLayoutTopBarTitle>
+      </PageLayoutTopBar>
+      <PageLayoutContent containerClassName="max-w-4xl">
+        <div className="flex flex-col gap-4" />
+      </PageLayoutContent>
+    </PageLayout>
+  );
+};
