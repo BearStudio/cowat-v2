0f157b1b29b4203037545080252a600392447c02
Noe TATOUD <noe.tatoud@gmail.com>
2026-02-20 20:14:27 +0100

    style: apply eslint auto-fix

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -4,10 +4,10 @@ export { FormFieldController } from './form-field-controller';
 export { FormFieldError } from './form-field-error';
 export { FormFieldHelper } from './form-field-helper';
 export { FormFieldLabel } from './form-field-label';
+export type { MultiStepFormStep } from './multi-step-form';
 export {
   MultiStepForm,
   MultiStepFormContent,
   MultiStepFormNavigation,
   useMultiStepForm,
 } from './multi-step-form';
-export type { MultiStepFormStep } from './multi-step-form';
