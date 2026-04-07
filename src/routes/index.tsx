ad3fa1fdf89316d025afe23fccfc96b8d16c1871
Noe TATOUD <noe.tatoud@gmail.com>
2026-04-07 22:29:37 +0200

    feat: add landing page with auth-based routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,25 @@
-import { createFileRoute, redirect } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { Activity } from 'react';
+
+import { Spinner } from '@/components/ui/spinner';
+
+import { authClient } from '@/features/auth/client';
+import { useRedirectAfterLogin } from '@/features/auth/utils';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
-  beforeLoad: () => {
-    throw redirect({ to: '/login' });
-  },
 });
 
 function RouteComponent() {
-  return null;
+  const session = authClient.useSession();
+  useRedirectAfterLogin();
+
+  return (
+    <>
+      {session.isPending && <Spinner full />}
+      <Activity mode={session.isPending ? 'hidden' : 'visible'}>
+        <Link to="/login">Login</Link>
+      </Activity>
+    </>
+  );
 }
